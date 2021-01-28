e99788527fc5f553a8c3dcfd24afa8f9687c10e9
Eduard Gorshkov <britvahp@gmail.com>
2021-01-28 17:01:09 +0400

    section 'calc' + media

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  const questions = document.querySelectorAll('.questions__question-title');
+  const questions = document.querySelectorAll('.questions__question-title'),
+        sum = document.querySelector('.calc__input--sum'),
+        time = document.querySelector('.calc__input--time');
+
+  // accordion
 
   questions.forEach(question => {
     question.addEventListener('click', () => {
@@ -8,5 +12,32 @@ window.addEventListener('DOMContentLoaded', () => {
       question.nextElementSibling.classList.toggle('questions__question-content--active');
     });
   });
+
+
+  // range
+
+  function range(input, progress, content) {
+    $(input).each(function(i, elem) {
+      val = $(this).val();
+      min = $(this).attr('min');
+      max = $(this).attr('max');
+      step = $(this).attr('step');
+      position = 100 / (max - step) * (val - step);
+      $(this).parent().find(progress).width(position + "%");
+    });
+    $(input).on('input', function() {
+        val = $(this).val();
+        min = $(this).attr('min');
+        max = $(this).attr('max');
+        step = $(this).attr('step');
+        position = 100 / (max - step) * (val - step);
+        $(this).parent().find(progress).width(position + "%");
+        content.value = val;
+    });
+  }
+
+  range('.range__input--sum', '.range__track--sum', sum);
+  range('.range__input--time', '.range__track--time', time);
+
 
 });
